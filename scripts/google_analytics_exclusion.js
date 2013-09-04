a5c1f5f8d7b1ffcea4c4ccfe8a83eefee713705e
PunchRockgroin <mrrockgroin@gmail.com>
2013-09-04 11:02:26 -0600

    Updated to latest source

--- scripts/google_analytics_exclusion.js
+++ scripts/google_analytics_exclusion.js
@@ -2,22 +2,26 @@
   Drupal.behaviors.gae = {
     attach: function (context, settings) {
       if (typeof _gaq !== 'undefined'){
-								var gaeExclude = $.cookie('gaeExclude');
-								if (typeof gaeExclude === 'undefined' || gaeExclude === null) {
-										$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
-												console.log(data.query);
-												if( ($.inArray(data.query, settings.gae.gaExclusionIPv4s) !== -1) ) {
-																// $.cookie('DRUPAL_VISITOR_IS', 'employee');
-																$.cookie('gaeExclude', settings.gae.gaExclusionKey, { expires: 30 });
-																_gaq.push(['_setVar', settings.gae.gaExclusionKey]);
-												}else{
-																	$.cookie('gaeExclude', 'anon', { expires: 30 });
-												}
-										});
-								}else if(gaeExclude === settings.gae.gaExclusionKey){
-												_gaq.push(['_setVar', settings.gae.gaExclusionKey]);
-								}		
-						}
+                var gaeExclude = $.cookie('gaeExclude');
+                var disableStr = 'ga-disable-' + settings.gae.gaProperty;
+                if (typeof gaeExclude === 'undefined' || gaeExclude === null) {
+                    $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+                        if( ($.inArray(data.query, settings.gae.gaExclusionIPv4s) !== -1) ) {
+                                window[disableStr] = true;
+                                $.cookie('gaeExclude', settings.gae.gaExclusionKey, { expires: 3600 });
+                                $.cookie(disableStr, true, { expires: 3600 });
+                                _gaq.push(['_setVar', settings.gae.gaExclusionKey]);
+
+                        }else{
+                                  $.cookie('gaeExclude', 'anon', { expires: 30 });
+                        }
+                    });
+                }else if(gaeExclude === settings.gae.gaExclusionKey){
+                        window[disableStr] = true;
+                        _gaq.push(['_setVar', settings.gae.gaExclusionKey]);
+
+                }
+            }
     }
   };
 })(jQuery);
